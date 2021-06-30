c68e1aa4d2f859a95d02c9f856cf9e5bd1f76518
prkwa <kawatam426@gmail.com>
2021-06-30 21:54:20 +0900

    [ストップウォッチ]を学習しました

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -13,3 +13,28 @@ const figure2 = document.getElementById("figure2")
 figure2.onclick = function() {
   figure2.classList.toggle("rounded")
 }
+//発展１
+const figure3 = document.getElementById("figure3")
+figure3.onmouseover = function() {
+  figure3.classList.toggle("rounded")
+}
+
+//発展２
+const figure4 = document.getElementById("figure4")
+
+// classList.toggle() を分解してみる。
+
+figure4.onclick = function() {
+  // console.log() で classListがどう適っているかを確認する。
+  // classList.valueにすると class="ここ" を見れる
+  console.log(figure4.classList)
+
+  if (figure4.classList.contains("circle") === true) {
+    figure4.classList.remove("circle")
+    figure4.classList.add("triangle")
+  } else if (figure4.classList.contains("triangle") === true) {
+    figure4.classList.remove("triangle")
+  } else {
+    figure4.classList.add("circle")
+  }
+}
