d35d4daf5b756a96cd19728348265d2833bf2fca
prkwa <kawatam426@gmail.com>
2021-07-29 00:23:06 +0900

    ATMデザイン作った

--- 02_レシピ/04 Vue ATM/main.js
+++ 02_レシピ/04 Vue ATM/main.js
@@ -1,7 +1,7 @@
 new Vue({
   el: "#app",
   data: {
-    inputValue: 0,
+    inputValue: "",
     balance: 0,
     logs: [],
   },
@@ -35,3 +35,35 @@ new Vue({
     },
   },
 })
+
+timerID = setInterval("clock()", 500) //0.5秒毎にclock()を実行
+
+function clock() {
+  document.getElementById("view_clock").innerHTML = getNow()
+}
+
+function getNow() {
+  var now = new Date()
+  var year = now.getFullYear()
+  var mon = now.getMonth() + 1 //１を足すこと
+  var day = now.getDate()
+  var hour = now.getHours()
+  var min = now.getMinutes()
+  var sec = now.getSeconds()
+
+  //出力用
+  var s =
+    year +
+    "年" +
+    mon +
+    "月" +
+    day +
+    "日" +
+    hour +
+    "時" +
+    min +
+    "分" +
+    sec +
+    "秒"
+  return s
+}
